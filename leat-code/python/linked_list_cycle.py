c9a59d08730083ac2c9a712526e4c058c8165df7
Hyuga-Tsukui <gakikiki407@gmail.com>
2022-06-21 00:00:16 +0900

    より早く

--- leat-code/python/linked_list_cycle.py
+++ leat-code/python/linked_list_cycle.py
@@ -1,11 +1,11 @@
 # Definition for singly-linked list.
+from typing import Optional
+
+
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
-
-from typing import Optional
-
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
@@ -14,26 +14,14 @@ class Solution:
             return False
         
         slow_pointer = head
-        fist_pointer = head
+        fist_pointer = head.next
         
-        while slow_pointer and slow_pointer.next and fist_pointer and fist_pointer.next:
-            tmp = fist_pointer.next
-            if tmp == head:
+        while slow_pointer and fist_pointer and slow_pointer.next:
+            if slow_pointer == fist_pointer:
                 return True
-
+            slow_pointer = slow_pointer.next 
+            tmp = fist_pointer.next
             if tmp:
                 fist_pointer = tmp.next
-            else:
-                return False
-            
-            if fist_pointer == head:
-                return True
-
-            if fist_pointer == slow_pointer:
-                return True
-            slow_pointer = slow_pointer.next
-
-            if slow_pointer == head:
-                return True
     
         return False
