801271cb80530a7c6faccb602087dac9932c9c01
Hyuga-Tsukui <gakikiki407@gmail.com>
2022-06-21 22:39:39 +0900

    detectCycle

--- leat-code/python/linked_list_cycle.py
+++ leat-code/python/linked_list_cycle.py
@@ -25,3 +25,32 @@ class Solution:
                 fist_pointer = tmp.next
     
         return False
+
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        if not head:
+            return
+        
+        pointer = head
+        
+        appearance_list = [pointer]
+        
+        while pointer and pointer.next:
+            pointer = pointer.next
+            if pointer in appearance_list:
+                return pointer
+            else:
+                appearance_list.append(pointer)
+        return
+
+if __name__ == '__main__':
+    obj = Solution()
+
+    node = ListNode(3)
+    start = ListNode(2)
+    node.next = start
+    node.next.next = ListNode(0)
+    node.next.next.next = ListNode(-4)
+    node.next.next.next.next =start
+
+    print(obj.detectCycle(node))
