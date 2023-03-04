cbffec55cd2debee76bd70dd0c9dd96951e2c63b
Hyuga-Tsukui <gakikiki407@gmail.com>
2023-03-04 11:27:51 +0900

    sandbox react query

--- react-location-sandbox/src/main.tsx
+++ react-location-sandbox/src/main.tsx
@@ -1,13 +1,33 @@
+import { QueryClient, QueryClientProvider, QueryFunction } from '@tanstack/react-query';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import {Router} from "./Router";
 
+import axios from "redaxios"
+
+const defaultQueryFn: QueryFunction = async ({ queryKey }) => {
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com${queryKey[0]}`,
+    )
+    return data
+  }
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            queryFn: defaultQueryFn
+        }
+    }
+})
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
         <Router>
             <App/>
         </Router>
+        </QueryClientProvider>
     </React.StrictMode>,
 )
