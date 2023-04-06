8a69ecced7d966d5e5c99dfeaa7857f3ec31909a
Hyuga-Tsukui <gakikiki407@gmail.com>
2023-04-06 23:00:21 +0900

    is prime number

--- c-playground/int.c
+++ c-playground/int.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
-int main(void) {
-    // 変換指定子 %d は整数を10進数で表示する
-    printf("curry %d yen\n", 400);
-    printf("curry %d yen\n", 400);
+
+/**
+ * 素数判定
+*/
+int isPrimeNumber(int n) {
+    if (n < 2) return 0;
+
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) return 0;
+    }
+
+    return 1;
 }
+
+int main(void) {
+    printf("%d\n", isPrimeNumber(7));
+    printf("%d\n", isPrimeNumber(10009));
+    printf("%d\n", isPrimeNumber(12));
+}
+
+
