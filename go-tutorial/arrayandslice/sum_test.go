14bb8315b88923b822bf49414717243d20f1d879
Hyuga-Tsukui <gakikiki407@gmail.com>
2023-05-05 21:46:51 +0900

    配列とスライス: カバレッジの取得 go test -cover

--- go-tutorial/arrayandslice/sum_test.go
+++ go-tutorial/arrayandslice/sum_test.go
@@ -4,16 +4,6 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
-		got := Sum(numbers)
-		expected := 15
-
-		if got != expected {
-			t.Errorf("expected %d but got %d given, %v", expected, got, numbers)
-		}
-	})
-
 	t.Run("collection of any numbers", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 		got := Sum(numbers)
